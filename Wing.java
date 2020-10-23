3c14357ce2af50c7061aed4dacb287f686e995af
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-23 16:04:36 +0530

    Eleventh commit

---
+++ Wing.java
@@ -0,0 +1,26 @@
+public class Wing{
+public static void main(String[] states)
+{
+System.out.println("JVM invoked main");
+System.out.println("states "+states);
+
+int sizeOfMainParam=states.length;
+        System.out.println("sizeOfMainParam "+sizeOfMainParam);
+        System.out.println("Element @ Index 0 "+states[0]);
+        System.out.println("states @ index 1 "+states[1]);
+		System.out.println("states @ index 2 "+states[2]);
+		System.out.println("states @ index 3 "+states[3]);
+		System.out.println("states @ index 4 "+states[4]);
+		System.out.println("states @ index 5 "+states[5]);
+		System.out.println("states @ index 6 "+states[6]);
+		System.out.println("states @ index 7 "+states[7]);
+		System.out.println("states @ index 8 "+states[8]);
+		System.out.println("states @ index 9 "+states[9]);
+		System.out.println("states @ index 10 "+states[10]);
+		System.out.println("states @ index 11 "+states[11]);
+		System.out.println("states @ index 12 "+states[12]);
+		System.out.println("states @ index 13"+states[13]);
+		System.out.println("states @ index 14 "+states[14]);
+		System.out.println("Exit main");
+		}
+	}
