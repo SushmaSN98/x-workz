a864aa64b178727f91c7ce0db6d0ca5414e8c6f6
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-23 23:14:09 +0530

    twelfth commit

---
+++ Pubg.java
@@ -0,0 +1,10 @@
+public class Pubg{
+public static void supply()
+{
+System.out.println("invoked supply from Pub-g class");
+}
+public static void enemiesAhead()
+{
+System.out.println("invoked enemiesAhead from Pub-g class");
+}
+}
