2a6cbc4731dc783a1cbd992e85fcdf2a505eebd1
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-24 07:47:43 +0530

    Twelfth commit

---
+++ Paint.java
@@ -0,0 +1,9 @@
+public class Paint{
+public static void brush(String brush)
+{
+System.out.println(" brush method is invoked");
+System.out.println("Colours 2:"+brush);
+
+System.out.println("brush method End");
+}
+}
