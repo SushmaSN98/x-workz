b7485d18da50332ed058c064cfd256e4b666e1cc
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-10-28 13:00:42 +0530

    Thirteenth commit

---
+++ Age.java
@@ -0,0 +1,15 @@
+public class Age{
+	
+	
+	public static void main(String[] wing)
+	{
+		
+		System.out.println("JVM invoked main()");
+		
+		String age=Stage.ageDefinition(56);
+		System.out.println("return value from ageDefinition()"+age);
+		
+		System.out.println("EXIT::main()");
+	}
+	}
+	
