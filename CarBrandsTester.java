247b3c230ab022da26353fff868e84cf220ff562
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-04 23:23:55 +0530

    Tester

---
+++ CarBrandsTester.java
@@ -0,0 +1,8 @@
+public class CarBrandsTester{
+public static void main(String[] getBrands)
+{
+System.out.println("invoked main method");
+CarBrands.getBrands();
+System.out.println("Exit :: main");
+}
+}
