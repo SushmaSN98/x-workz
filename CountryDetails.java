db07db674aef354a9ef981c0649b8189da8fe7dc
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-05 23:28:33 +0530

    countryDetails

---
+++ CountryDetails.java
@@ -0,0 +1,22 @@
+public class CountryDetails
+{
+public static void main(String[] args)
+{
+
+Country.name="INDIA";
+System.out.println("name of country:"+ Country.name);
+
+Country.arts="Dance";
+System.out.println("arts of country:"+ Country.arts);
+
+Country.festivals="diwali";
+System.out.println("festivals of country:"+ Country.festivals);
+
+Country.numberOfStates=28;
+System.out.println("numberOfStates of country:"+ Country.numberOfStates);
+
+Country.numberOfTerritories=7;
+System.out.println("numberOfTerritories of country:"+ Country.numberOfTerritories );
+
+}
+}
