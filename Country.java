f5617effd2ec65800fccb167a1cea8d3e7fc9753
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-05 23:27:34 +0530

    Country

---
+++ Country.java
@@ -0,0 +1,8 @@
+public class Country
+{
+public static String name; 
+public static String arts;
+public static String festivals;
+public static byte numberOfStates;
+public static byte numberOfTerritories;
+}
