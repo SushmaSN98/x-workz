3eadf750cc5cb247a12b50499fd6900721041282
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-08 22:00:56 +0530

    Switch and Enum

---
+++ InstituteTester.java
@@ -0,0 +1,17 @@
+public class InstituteTester{
+
+
+public static void main(String[] photos)
+{
+
+System.out.println("invoked main, doing switch and enum");
+
+InstituteFinder.lowestAndBestUsingString("ABC");
+	String instituteName="ABC";
+	Institute enumInstituteName=Institute.valueOf(instituteName);
+InstituteFinder.lowestAndBestUsingEnum(enumInstituteName);
+
+System.out.println("EXIT :: main, doing switch and enum");
+}
+
+}
