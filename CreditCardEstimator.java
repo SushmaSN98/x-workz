2d4fb352ce2b47c3ae40d061077331d305402740
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-12 21:13:37 +0530

    credit

---
+++ CreditCardEstimator.java
@@ -0,0 +1,20 @@
+package com.Xworkz.basics.credit;
+
+public class CreditCardEstimator {
+private static double limit=1000;
+public static void expenditure(double amount) {
+	System.out.println("total amount in credit card is "+limit);
+	if(amount<limit) {
+	System.out.println("total expenditure is "+amount);
+	amount=limit-amount;
+	System.out.println("remaining amount is "+amount);
+}
+	else {
+		System.out.println("expenditure must be less than limit");
+	}
+}
+}
+
+
+
+
