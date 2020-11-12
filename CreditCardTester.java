cdb94914cd620a93657a76dc5cfa50436fdf69e1
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-12 21:14:11 +0530

    credit

---
+++ CreditCardTester.java
@@ -0,0 +1,11 @@
+package com.Xworkz.basics.credit;
+
+public class CreditCardTester {
+
+	public static void main(String[] args) {
+		System.out.println("invoked main method");
+		CreditCardEstimator.expenditure(120);
+
+	}
+
+}
