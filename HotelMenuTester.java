cd8aa3a690f91aff9b99c25c5760185af2f9126a
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-13 23:16:24 +0530

    hotel

---
+++ HotelMenuTester.java
@@ -0,0 +1,13 @@
+package com.Xworkz.hotel;
+
+public class HotelMenuTester {
+public static void main(String[] args) {
+		
+		System.out.println("Total items to search " + HotelMenuSearch.totalFoodItems());
+		HotelMenuSearch.search("CoolDrinks");
+		
+	}
+
+}
+
+
