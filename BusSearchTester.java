b638db4cb841ab6b3a15f6b62b0aee4ed305485a
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-13 23:14:44 +0530

    place

---
+++ BusSearchTester.java
@@ -0,0 +1,20 @@
+package com.Xworkz.place;
+
+public class BusSearchTester {
+	public static void main(String[] args) {
+	
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total places to search" +BusSearch.totalPlaces());
+		BusSearch.search("Tirupathi");
+
+		System.out.println("main nirgamana");
+
+	}
+
+
+
+}
+
+
+
