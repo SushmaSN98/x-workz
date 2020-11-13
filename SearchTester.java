4d03ba2307644c244772d5a4e25494a3e8ebd386
SushmaSN98 <72965040+SushmaSN98@users.noreply.github.com>
2020-11-13 23:13:51 +0530

    online

---
+++ SearchTester.java
@@ -0,0 +1,16 @@
+package com.Xworkz.online;
+
+public class SearchTester {
+	public static void main(String[] args) {
+
+		System.out.println("jvm kareyuvudu main");
+
+		System.out.println("Total items to search" + Amazon.totalItems());
+		Amazon.search("Laptop");
+
+		System.out.println("main nirgamana");
+
+}
+}
+
+
